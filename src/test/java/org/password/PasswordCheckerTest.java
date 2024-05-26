4d3b71e265d22ff639e0f9c7f31cb56f76797937
Olga <aleksandrova1olga@gmail.com>
2024-05-26 16:09:45 +0300

    Password Check Unit Tests

--- src/test/java/org/password/PasswordCheckerTest.java
+++ src/test/java/org/password/PasswordCheckerTest.java
@@ -1,6 +1,8 @@
 package org.password;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PasswordCheckerTest {
@@ -8,5 +10,35 @@ public class PasswordCheckerTest {
     @Test
     void shouldReturnTrueForComplexPasswordWithDigitsAndSpecialChars() {
          assertTrue( PasswordChecker.checkPasswordComplexity("Password123!") );
+    }
+
+    @Test
+    void shouldReturnFalseIfPasswordHasOnlyDigits () {
+        assertFalse( PasswordChecker.checkPasswordComplexity("12345678"));
+    }
+
+    @Test
+    void shouldReturnFalseIfPasswordHasNoSpecialChars () {
+        assertFalse( PasswordChecker.checkPasswordComplexity("Password123"));
+    }
+
+    @Test
+    void shouldReturnTrueIfPasswordLong () {
+        assertTrue( PasswordChecker.checkPasswordComplexity("PasswordIsTooLong1234567890....."));
+    }
+
+    @Test
+    void shouldReturnFalseIfPasswordIsShort () {
+        assertFalse( PasswordChecker.checkPasswordComplexity("Pa1!") );
+    }
+
+    @Test
+    void shouldReturnFalseIfPasswordHasOnlySpecialChars () {
+        assertFalse( PasswordChecker.checkPasswordComplexity(".!@'<,.>?"));
+    }
+
+    @Test
+    void shouldReturnFalseIfPasswordHasOnlyLetters () {
+        assertFalse( PasswordChecker.checkPasswordComplexity("Password"));
     }
 }
